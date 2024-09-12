068ecfbaebaa71317d4732b333fad36b43acc307
barkeleyrico <barkeley.rico@gmail.com>
2024-09-12 20:28:23 +0800

    add infra files

---
+++ infrastructure/terraform.tf
@@ -0,0 +1,22 @@
+provider "aws" {
+  region     = "ap-southeast-1"
+  access_key = "AKIAT4GVRUUS2TAMXLPP"
+  secret_key = "4rk3dGtKu08r/WkzrloSxmk60JMfuMZ00hsRsp+Z"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-04a5ce820a419d6da"
+  instance_type = "t2.micro"               # Type of EC2 instance
+
+  # Optionally, add a tag to identify the instance
+  tags = {
+    Name = "terraform"
+  }
+
+  # Optional: Configure security group
+  # vpc_security_group_ids = [aws_security_group.example.id]
+
+  # Optional: Configure key pair (for SSH access)
+  # key_name = "your-key-pair-name"  # Change to your key pair name
+}
+
